0d68b56aaa35497384387b183f22b21bd85db372
Hülya <hulyacetin12@gmail.com>
2017-08-16 16:18:23 +0300

    Firebase bağlantısı yapıldı

---
+++ Yemekhane/app/src/main/java/com/example/hulya/yemekhane/dummydata/FirebaseDataList.java
@@ -0,0 +1,28 @@
+package com.example.hulya.yemekhane.dummydata;
+
+/**
+ * Created by hulya on 16.08.2017.
+ */
+
+public class FirebaseDataList {
+
+    public String Soup1, Soup2, Entree1, Entree2, MainFood1, MainFood2, MainFood3;
+    public String Alternatif1, Alternatif2, Alternatif3, Date;
+
+
+    public FirebaseDataList(String Soup1, String Soup2, String Entree1, String Entree2,
+                            String MainFood1, String MainFood2, String MainFood3,
+                            String Alternatif1, String Alternatif2, String Alternatif3, String Date) {
+        this.Alternatif1 = Alternatif1;
+        this.Alternatif2 = Alternatif2;
+        this.Alternatif3 = Alternatif3;
+        this.Date = Date;
+        this.Entree1 = Entree1;
+        this.Entree2 = Entree2;
+        this.MainFood1 = MainFood1;
+        this.MainFood2 = MainFood2;
+        this.MainFood3 = MainFood3;
+        this.Soup1 = Soup1;
+        this.Soup2 = Soup2;
+    }
+}
--- Yemekhane/app/src/main/java/com/example/hulya/yemekhane/ui/RecyclerViewActivity.java
+++ Yemekhane/app/src/main/java/com/example/hulya/yemekhane/ui/RecyclerViewActivity.java
@@ -13,26 +13,32 @@ import android.widget.TextView;
 
 import com.example.hulya.yemekhane.R;
 import com.example.hulya.yemekhane.adapter.FoodListAdapter;
+import com.example.hulya.yemekhane.dummydata.FirebaseDataList;
 import com.example.hulya.yemekhane.dummydata.GenerateDummyData;
 import com.example.hulya.yemekhane.viewmodel.FoodListVM;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.Firebase;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
  * Created by hulya on 14.08.2017.
  */
 
-public class RecyclerViewActivity extends AppCompatActivity {
+public class RecyclerViewActivity extends AppCompatActivity implements ChildEventListener {
 
     Toolbar toolbar;
     private RecyclerView rFoodList=null;
     private ArrayList<FoodListVM> foodList = null;
     private TextView txtDateInformation;
-
+    private Firebase foodListRef;
+    private Map<String, FirebaseDataList> firebaseDataListMap;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +48,25 @@ public class RecyclerViewActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
 
+        Firebase.setAndroidContext(this);
+        foodListRef = new Firebase("https://refectory-84b81.firebaseio.com/");
+        foodListRef.child("Foods");
+        firebaseDataListMap = new HashMap<String, FirebaseDataList>();
+        firebaseDataListMap.put("Day1", new FirebaseDataList("Naneli Dövme Çorba", "Ev Usulü Tel Şehriye Çorba", "Nohutlu Pilav", "Soslu Makarna", "Domates Soslu Köfte", "Fajita Soslu Tavuk Kavurma",
+                "Bamya", "İzmir Kumru", "Ayran", "Special Salata", "16.08.2017"));
+        firebaseDataListMap.put("Day2", new FirebaseDataList("Tarhana Çorbası", "Kabak Çorbası", "Nohutlu Pilav", "Soslu Makarna", "Domates Soslu Köfte", "Fajita Soslu Tavuk Kavurma",
+                "Bamya", "İzmir Kumru", "Ayran", "Special Salata", "17.08.2017"));
+        firebaseDataListMap.put("Day3", new FirebaseDataList("Naneli Dövme Çorba", "Ev Usulü Tel Şehriye Çorba", "Nohutlu Pilav", "Soslu Makarna", "Domates Soslu Köfte", "Fajita Soslu Tavuk Kavurma",
+                "Bamya", "İzmir Kumru", "Ayran", "Special Salata", "18.08.2017"));
+        firebaseDataListMap.put("Day4", new FirebaseDataList("Naneli Dövme Çorba", "Ev Usulü Tel Şehriye Çorba", "Nohutlu Pilav", "Soslu Makarna", "Domates Soslu Köfte", "Fajita Soslu Tavuk Kavurma",
+                "Bamya", "İzmir Kumru", "Ayran", "Special Salata", "19.08.2017"));
+        firebaseDataListMap.put("Day5", new FirebaseDataList("Naneli Dövme Çorba", "Ev Usulü Tel Şehriye Çorba", "Nohutlu Pilav", "Soslu Makarna", "Domates Soslu Köfte", "Fajita Soslu Tavuk Kavurma",
+                "Bamya", "İzmir Kumru", "Ayran", "Special Salata", "20.08.2017"));
+        firebaseDataListMap.put("Day6", new FirebaseDataList("Naneli Dövme Çorba", "Ev Usulü Tel Şehriye Çorba", "Nohutlu Pilav", "Soslu Makarna", "Domates Soslu Köfte", "Fajita Soslu Tavuk Kavurma",
+                "Bamya", "İzmir Kumru", "Ayran", "Special Salata", "21.08.2017"));
+        firebaseDataListMap.put("Day7", new FirebaseDataList("Naneli Dövme Çorba", "Ev Usulü Tel Şehriye Çorba", "Nohutlu Pilav", "Soslu Makarna", "Domates Soslu Köfte", "Fajita Soslu Tavuk Kavurma",
+                "Bamya", "İzmir Kumru", "Ayran", "Special Salata", "22.08.2017"));
+        foodListRef.setValue(firebaseDataListMap);
 
         initView();
         DateInformation();
@@ -63,13 +88,21 @@ public class RecyclerViewActivity extends AppCompatActivity {
         rFoodList.setAdapter(foodListAdapter);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        foodListRef.child("Day1");
+
+    }
 
     public void DateInformation() {
         Date date = new Date();
         DateFormat format = new SimpleDateFormat("EEEE-dd/MM/yyyy");
         txtDateInformation.setText(format.format(date).toString());
     }
-    }
+
+
+}
 
 
 
